f616a659b49373fad4a3eb0798e7fb1451d7d9ec
io <120751005+ip127@users.noreply.github.com>
2024-09-23 00:00:13 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,13 @@
 
 export let PORT = 370; // 端口
 
-export interface Proxy {
+export interface Proxy
+    {
     domain: string;
     url: string;
-    enable: boolean;
-    replaces?: {
-        old: string,
-        new: string
-    }[];
 }
+
+
 
 // 代理
 export let proxys: Proxy[] = [
