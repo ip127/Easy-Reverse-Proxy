94da99c0711e68da1d1595dc0fba72db90eb9245
io <120751005+ip127@users.noreply.github.com>
2024-09-22 23:45:51 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,25 +1,13 @@
-export let PORT = 370; // 端口
-
-export interface Proxy {
+export interface Proxy
+    {
     domain: string;
     url: string;
-    enable: boolean;
-    replaces?: {
-        old: string,
-        new: string
-    }[];
 }
 
 // 代理
 let PROXYS: Proxy[] = [
     {
         domain: "easy-reverse-proxy-xi.vercel.app",
-        url: "https://www.microsoft.com/",
-        enable: true
-    },
-    {
-        domain: "easy-reverse-proxy-xi.vercel.app",
-        url: "https://www.google.com/",
-        enable: true
+        url: "https://www.google.com/"
     }
-]
+];
