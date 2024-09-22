f672f6431dae82bf18942021f4ed54c59891b9a3
io <120751005+ip127@users.noreply.github.com>
2024-09-22 23:54:21 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export interface Proxy
 // 代理
 export let proxys: Proxy[] = [
     {
-        domain: "easy-reverse-proxy-xi.vercel.app",
+        domain: "easy-reverse-proxy.vercel.app",
         url: "https://www.google.com/"
     }
 ];
